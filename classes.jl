347bba217a45488c234ca8b2d6fbe4d83f4f9a27
Xanatc <alexandrasprodrigues@hotmail.com>
2023-03-31 17:06:27 +0100

    Nao sei como funciona o reader, writer e initform

--- classes.jl
+++ classes.jl
@@ -23,7 +23,59 @@ global Object = class(:Object, [], Dict())
 #class_registry[:Object] = Object
 
 function defclass(name::Symbol, direct_superclasses, direct_slots)
-    slots_dict = Dict(slot => nothing for slot in direct_slots)
+    #slots_dict = Dict(slot => nothing for slot in direct_slots)
+    #println(direct_slots)
+    slots_dict = Dict()
+
+    for slot in direct_slots
+        # only slot name is provided
+        if isa(slot, Symbol)
+            println("Is a symbol")
+            slots_dict[slot] = nothing
+        # slot name includes reader, writer or initform
+        elseif isa(slot, Vector)
+            println("Is a Vector")
+            slot_name = slot[1]
+            println(slot_name)
+            slot_options = slot[2:end]
+            println(slot_options)
+
+            if isa(slot_name, Symbol)
+                println("Vector slot name is a symbol")
+                slots_dict[slot_name] = nothing
+            else
+                println("Vector with Name and Value")
+                #slots_dict[slot] = direct_slots[slot]
+                slots_dict[slot_name.first] = slot_name.second
+            end
+            
+            for option in slot_options
+                #if haskey(slot_options, :initform)
+                if :initform in option
+                    println("Vector slot name as initform")
+                    #slots_dict[slot_name] = slot_options[:initform]
+                    slots_dict[slot_name] = option.second
+                #if haskey(slot_options, :reader)
+                elseif :reader in option
+                    println("Created a reader method")
+        
+                #if haskey(slot_options, :writer)
+                elseif :writer in option
+                    println("Created a writer method")
+                end
+            end
+
+        # slot name with iniform value are provided
+        else 
+            println("Name and Value")
+            #slots_dict[slot] = direct_slots[slot]
+            #doing it like a pair
+            slots_dict[slot.first] = slot.second
+        end
+    end
+    
+    
+    #slots_dict = Dict(direct_slots[1] => nothing for slot in direct_slots)
     
     new_superclasses = Vector{class}()
     
@@ -113,16 +165,20 @@ c1.real
 c1.imag
 c1.imag += 3
 
+#global CountingClass = defclass(:CountingClass, [Class], [counter=0])
+global CountingClass = defclass(:CountingClass, [Class], [Pair(:counter, 0)])
+global ColorMixin = defclass(:ColorMixin, [], [[:color, Pair(:reader, :get_color), Pair(:writer, :set_color!), Pair(:initform, "ola")]])
+
+
 global Line = defclass(:Line, [Shape], [:from, :to])
 global Circle = defclass(:Circle, [Shape], [:center, :radius])
 
 global Screen = defclass(:Screen, [Device], [])
 global Printer = defclass(:Printer, [Device], [])
 
-global Person = defclass(:Person, [], [[:name, reader=get_name, writer=set_name!],
-[:age, reader=get_age, writer=set_age!, initform=0],
-[:friend, reader=get_friend, writer=set_friend!]],
-metaclass=UndoableClass)
+global Foo = defclass(:Foo, [], [], metaclass=CountingClass)
+
+global Bar = defclass(:Bar, [], [], metaclass=CountingClass)
 
 global ColorMixin = defclass(:ColorMixin, [], [[:color, reader=get_color, writer=set_color!]])
 
@@ -131,11 +187,10 @@ global ColoredCircle = defclass(:ColoredCircle, [ColorMixin, Circle], [])
 
 global ColoredPrinter = defclass(:ColoredPrinter, [Printer], [[ink=:black, reader=get_device_color, writer=_set_device_color!]])
 
-global CountingClass = defclass(:CountingClass, [Class], [counter=0])
-
-global Foo = defclass(:Foo, [], [], metaclass=CountingClass)
-
-global Bar = defclass(:Bar, [], [], metaclass=CountingClass)
+global Person = defclass(:Person, [], [[:name, reader=get_name, writer=set_name!],
+[:age, reader=get_age, writer=set_age!, initform=0],
+[:friend, reader=get_friend, writer=set_friend!]],
+metaclass=UndoableClass)
 
 #=
 function defgeneric(name::Symbol, args...)
